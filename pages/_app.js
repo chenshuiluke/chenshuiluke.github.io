5e1499cbde107759cccaae7abae78aa323a53628
Luke Chen Shui <chenshuiluke@gmail.com>
2021-08-31 00:21:27 -0500

    Basically finished rewrite

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import "@fontsource/noto-sans";
+import "@fontsource/ubuntu";
+import "@fontsource/bebas-neue";
+import "@fontsource/permanent-marker";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

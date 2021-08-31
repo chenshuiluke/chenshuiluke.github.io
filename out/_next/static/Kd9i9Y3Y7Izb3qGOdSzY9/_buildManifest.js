f854f175e2d759f1a1918935cef740a00c54cd3c
Luke Chen Shui <chenshuiluke@gmail.com>
2021-08-31 01:36:01 -0500

    Added out folder

---
+++ out/_next/static/Kd9i9Y3Y7Izb3qGOdSzY9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ddc9e94f-f1e6d55076587e6c3f65.js","static/css/5e18b62a540cd23738e6.css","static/chunks/pages/index-a83130fd4bdbdabb6712.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

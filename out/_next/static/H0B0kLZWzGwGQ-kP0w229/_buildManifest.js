013e77bcc59e9d1b0888f56a8dc02a518e345423
lukecharta <Luke@getcharta.com>
2024-03-09 23:30:08 -0500

    Converted to NextJS

--- out/_next/static/H0B0kLZWzGwGQ-kP0w229/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ddc9e94f-f1e6d55076587e6c3f65.js","static/css/5e18b62a540cd23738e6.css","static/chunks/pages/index-bfbd60d01255b8ab3bed.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import WavyLine from "./components/wavy_line/wavy_line";
+
+export default function Home() {
+  return (
+    <main className="">
+      <WavyLine />{" "}
+    </main>
+  );
+}

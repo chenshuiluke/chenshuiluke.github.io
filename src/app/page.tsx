0418b8d53cd5a07a41a7563a155f2760feca9178
Luke Chen Shui <chenshuiluke@gmail.com>
2024-03-16 12:11:18 -0500

    Committing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import WavyLine from "./components/wavy_line/wavy_line";
 import Nav from "./components/nav/nav";
 import Cloud from "./components/cloud/cloud";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
       <Nav />
       <div className="space-section rounded-lg mx-20">
+        {/* <Image className="corner-border" src={'/images/corner.png'} width={200} height={200} alt="Cloud corner"/> */}
         <Cloud />
+        
+
       </div>
     </main>
   );

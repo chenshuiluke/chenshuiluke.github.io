ebacfc4139338fe55cdfe50650d622f5a7aa12b3
Luke Chen Shui <chenshuiluke@gmail.com>
2026-04-02 13:31:40 -0500

    Insert Beacon AI loader script in layout
    Added a script tag for the Beacon AI loader.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <script
+  id="beacon-ai-loader"
+  src="https://dev-beacon-ai.vercel.app/api/widget/loader.js"
+  data-chatbot-id="556ac132-73e5-4140-9121-140d4b15a608"
+  data-api-key="8931772dd236204427d036302259c5d0b237756cbeb2b8e7400f8465d284ade9"
+></script>
     </html>
   );
 }
